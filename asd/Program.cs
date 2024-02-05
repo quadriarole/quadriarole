fc134a13dcf20c60ffabc036c05552e007276455
unknown <quadriarole@gmail.com>
2024-02-05 12:19:36 +0000

    bad

---
+++ asd/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.Json;
+using asd;
+
+User user = new User
+{
+    Name = "ade",
+    Email = "ade@gmail.com",
+    Password = "1234",
+    Role = "Student",
+};
+
+string a = JsonSerializer.Serialize(user);
+System.Console.WriteLine(a);
+
+User b = JsonSerializer.Deserialize<User>(a);
+System.Console.WriteLine(b.Name);
---
+++ asd/User.cs
@@ -0,0 +1,10 @@
+namespace asd
+{
+    public class User
+    {
+        public string Name{get;set;}
+        public string Email{get;set;}
+        public string Password{get;set;}
+        public string Role{get;set;}
+    }
+}
